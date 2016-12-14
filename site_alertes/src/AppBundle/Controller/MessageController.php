a15a2d6edf46fce01d5d2b298a2e252fc6afaa4d
Guilhaume Moraud <moraudguilhaume@gmail.com>
2016-12-14 01:01:11 +0100

    Model Message + formulaire

---
+++ site_alertes/src/AppBundle/Controller/MessageController.php
@@ -0,0 +1,52 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Message;
+
+
+
+
+
+  class MessageController extends Controller
+  {
+    /**
+    *@Route("/nouveau-message")
+    *@Template("message/nouveau-message.html.twig")
+    */
+    public function créerMessage(Request $request){
+      $nouveauMessage = new Message();
+      //J'utilise un form builder pour créer un formulaire  ('form') qui correspond à une entité message
+      $formBuilder = $this->get('form.factory')->createBuilder('form', $nouveauMessage);
+      //J'ajout les champs que je veux dans mon futur formualaire
+      $formBuilder
+        ->add('objet',      'text')
+        ->add('contenu',    'textarea')
+        ->add('créer',      'submit')
+      ;
+      //Je construit le formualaire
+      $form = $formBuilder->getForm();
+      //// On fait le lien Requête <-> Formulaire
+      $form->handleRequest($request);
+      if($form->isValid()){
+        $managerEntite = $this->getDoctrine()->getManager();
+        $managerEntite->persist($nouveauMessage);
+        $managerEntite->flush();
+        //rediriger sur l'acueil quand on en aura un
+      }
+      return array('form' => $form->createView());
+    }
+
+
+
+
+      public function indexAction(Request $request)
+      {
+          return $this->render('index.html.twig');
+      }
+  }
+
+ ?>
---
+++ site_alertes/src/AppBundle/Entity/Message.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Message
+ *
+ * @ORM\Table(name="message")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="objet", type="string", length=255)
+     */
+    private $objet;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $contenu;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idMessage
+     *
+     * @param string $idMessage
+     *
+     * @return Message
+     */
+
+
+    /**
+     * Set objet
+     *
+     * @param string $objet
+     *
+     * @return Message
+     */
+    public function setObjet($objet)
+    {
+        $this->objet = $objet;
+
+        return $this;
+    }
+
+    /**
+     * Get objet
+     *
+     * @return string
+     */
+    public function getObjet()
+    {
+        return $this->objet;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Message
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+}
