1352147094acd63e394e4fe1c24d8b6bcedb1621
Guilhaume <moraudguilhaume@gmail.com>
2016-12-24 06:27:54 +0100

    Ajout des imports manquants dans le GroupeController pour les annotations ROUTE et TEMPLATE

--- site_alertes/src/AppBundle/Controller/GroupeController.php
+++ site_alertes/src/AppBundle/Controller/GroupeController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace AppBundle\Controller;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class GroupeController extends Controller {
