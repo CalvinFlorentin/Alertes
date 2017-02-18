f1d0bac0fa5ca08bbc626b38c831dcb56176a7b7
Djabiri Abalkassim <abalkass@hotmail.fr>
2017-02-18 21:10:20 +0100

    Affiche listes des protocoles

--- site_alertes/src/AppBundle/Controller/ProtocoleController.php
+++ site_alertes/src/AppBundle/Controller/ProtocoleController.php
@@ -2,23 +2,79 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Protocole;
+//use AppBundle\Entity\ProtocoleInstruction;
+//use AppBundle\Entity\Instruction;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * Instruction controller.
+ * Protocole controller.
  *
  * @Route("protocole")
  */
 class ProtocoleController extends Controller
 {
     /**
+     * Displays procotocole's menu
+     *
      * @Route("/", name="protocole_index")
      */
     public function indexAction()
     {
         return $this->render('protocole/index.html.twig');
     }
+
+    /**
+     * Lists all protocoles entities.
+     *
+     * @Route("/lists", name="protocole_list")
+     * @Method("GET")
+     */
+    public function showAllAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $protocoles = $em->getRepository('AppBundle:Protocole')->findAll();
+        /*$protocoleInstruction = $em->getRepository('AppBundle:ProtocoleInstruction')->findAll();
+        $instructions = $em->getRepository('AppBundle:Instruction')->findAll();*/
+
+        return $this->render('protocole/listAll.html.twig', array(
+            'protocoles' => $protocoles,
+        ));
+
+    }
+
+    /**
+     * Creates a new instruction entity.
+     *
+     * @Route("/new", name="protocole_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction() {
+        return $this->render('protocole/new.html.twig');
+    }
+
+    /**
+     * Finds and displays a protocole entity.
+     *
+     * @Route("/{id}", name="protocole_show")
+     * @Method("GET")
+     */
+    public function showAction() {
+        return $this->render('protocole/show.html.twig');
+    }
+
+    /**
+     * Displays a form to edit an existing protocole entity.
+     *
+     * @Route("/{id}/edit", name="protocole_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction() {
+        return $this->render('protocole/edit.html.twig');
+    }
+
+
 }
---
+++ site_alertes/src/AppBundle/Form/ProtocoleType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProtocoleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nomProtocole')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Protocole'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_protocole';
+    }
+
+
+}
